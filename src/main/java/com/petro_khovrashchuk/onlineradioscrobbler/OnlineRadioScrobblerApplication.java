98b9ff3372aee40a2413d44ea798ebe38ff688ab
Petro_Khovrashchuk <petro_khovrashchuk@epam.com>
2020-08-30 12:54:04 +0300

    init commit

---
+++ src/main/java/com/petro_khovrashchuk/onlineradioscrobbler/OnlineRadioScrobblerApplication.java
@@ -0,0 +1,13 @@
+package com.petro_khovrashchuk.onlineradioscrobbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRadioScrobblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRadioScrobblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petro_khovrashchuk/onlineradioscrobbler/OnlineRadioScrobblerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petro_khovrashchuk.onlineradioscrobbler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRadioScrobblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
